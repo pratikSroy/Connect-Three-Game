45fca65ba858787f14a17118b03a8c17f6d2260d
pratikSroy <pratik.sr99@gmail.com>
2022-04-04 15:44:15 +0530

    layout upload

--- app/src/main/java/com/example/connect3game/MainActivity.java
+++ app/src/main/java/com/example/connect3game/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.connect3game;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void dropIn(View view) {
+        ImageView counter = (ImageView) view;
+
+        counter.setTranslationY(-1500);
+
+        counter.setImageResource(R.drawable.yellow);
+
+        counter.animate().translationYBy(1500).setDuration(300);
     }
 }
